c5fffd8f331d32498c18bd6bde0ed6348c7a6942
skaftisveins <skaftisveins@gmail.com>
2020-08-18 12:20:12 +0000

    create post container widget done

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
+
+import '../config/palette.dart';
+import '../data/data.dart';
+import '../widgets/widgets.dart';
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            brightness: Brightness.light,
+            backgroundColor: Colors.white,
+            title: Text(
+              'facebook',
+              style: TextStyle(
+                  color: Palette.facebookBlue,
+                  fontSize: 28,
+                  fontWeight: FontWeight.bold,
+                  letterSpacing: -1.2),
+            ),
+            centerTitle: false,
+            floating: true,
+            actions: [
+              CircleButton(
+                icon: Icons.search,
+                iconSize: 30,
+                onPressed: () => print('Search'),
+              ),
+              CircleButton(
+                icon: MdiIcons.facebookMessenger,
+                iconSize: 30,
+                onPressed: () => print('Messenger'),
+              ),
+            ],
+          ),
+          SliverToBoxAdapter(
+            child: CreatePostContainer(
+              currentUser: currentUser,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/widgets/create_post_container.dart
@@ -0,0 +1,80 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/material.dart';
+
+import '../models/models.dart';
+
+class CreatePostContainer extends StatelessWidget {
+  final User currentUser;
+
+  const CreatePostContainer({Key key, @required this.currentUser})
+      : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.fromLTRB(12, 8, 12, 0),
+      color: Colors.white,
+      child: Column(
+        children: [
+          Row(
+            children: [
+              CircleAvatar(
+                radius: 20,
+                backgroundColor: Colors.grey[200],
+                backgroundImage:
+                    CachedNetworkImageProvider(currentUser.imageUrl),
+              ),
+              const SizedBox(
+                width: 10,
+              ),
+              Expanded(
+                child: TextField(
+                  decoration: InputDecoration.collapsed(
+                      // removes line under text
+                      hintText: 'What\'s on your mind?'),
+                ),
+              ),
+            ],
+          ),
+          const Divider(
+            height: 10,
+            thickness: 0.5,
+          ),
+          Container(
+            height: 40,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                FlatButton.icon(
+                  onPressed: () => print('Live'),
+                  icon: Icon(
+                    Icons.videocam,
+                    color: Colors.red,
+                  ),
+                  label: Text('Live'),
+                ),
+                const VerticalDivider(width: 8),
+                FlatButton.icon(
+                  onPressed: () => print('Photo'),
+                  icon: Icon(
+                    Icons.photo_library,
+                    color: Colors.green,
+                  ),
+                  label: Text('Photo'),
+                ),
+                const VerticalDivider(width: 8),
+                FlatButton.icon(
+                  onPressed: () => print('Room'),
+                  icon: Icon(
+                    Icons.video_call,
+                    color: Colors.purpleAccent,
+                  ),
+                  label: Text('Room'),
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
